8a0e4a4ad548b3de0d732d4aad3aa211c3be2649
Orellana99 <48233706+Orellana99@users.noreply.github.com>
2019-04-03 21:45:27 -0600

    Add files via upload

---
+++ Clase0204/Clase0204/Extra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase0204
+{
+    public partial class Extra : Form
+    {
+        public Extra()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 Reg = new Form1();
+            Reg.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Clase0204/Clase0204/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase0204
+{
+    public partial class Form1 : Form
+    {
+        Extra fr1 = new Extra();
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar2.Maximum = 100;
+            progressBar2.Minimum = 0;
+            progressBar2.Step = 4;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i <= 24; i++) {
+                progressBar2.PerformStep();
+                this.Hide();
+
+            }
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 24; i++)
+            {
+                progressBar2.PerformStep();
+              //  fr1.Show();
+              //  this.Hide();
+                button4.BackColor = Color.Green;
+                timer1.Interval = 3000;
+                timer1.Enabled = true;
+ 
+
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //if (timer2. )
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.Green;
+            timer1.Interval = 3000;
+            timer1.Enabled = true;
+ 
+                
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Extra frm = new Extra();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
